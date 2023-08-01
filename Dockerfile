ded61fce1533a8a0efa0b4174a9c5fd426a3d45e
Aruljothi <aruljothi@gmail.com>
2023-08-01 11:34:32 +0530

    spring docker code

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:8
+EXPOSE 8088
+ADD target/Springwithdb-0.0.1-SNAPSHOT.war Springwithdb-0.0.1-SNAPSHOT.war
+ENTRYPOINT [ "java","-jar","/Springwithdb-0.0.1-SNAPSHOT.war" ]
---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int empno;
+	private String empname;
+	private String empemail;
+	private int age;
+	
+	
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public String getEmpname() {
+		return empname;
+	}
+
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+
+	public String getEmpemail() {
+		return empemail;
+	}
+
+	public void setEmpemail(String empemail) {
+		this.empemail = empemail;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empno=" + empno + ", empname=" + empname + ", empemail=" + empemail + ", age=" + age + "]";
+	}
+
+
+}
